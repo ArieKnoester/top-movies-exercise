0bc3c03cd351898bd91c2904b746a0dceb7397a6
Arie Knoester <arikstr@gmail.com>
2024-02-05 13:59:44 -0600

    Created model for database table.

---
+++ models/movie.py
@@ -0,0 +1,22 @@
+# https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(275), unique=True, nullable=False)
+    year: Mapped[str] = mapped_column(String(4), nullable=False)
+    description: Mapped[str] = mapped_column(String(275), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(275), nullable=False)
