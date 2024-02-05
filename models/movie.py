a45e2107e71c9c7348b87c5e1edc34f89eddea38
Arie Knoester <arikstr@gmail.com>
2024-02-05 15:42:42 -0600

    Increased max characters for description column.

--- models/movie.py
+++ models/movie.py
@@ -15,7 +15,7 @@ class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(275), unique=True, nullable=False)
     year: Mapped[str] = mapped_column(String(4), nullable=False)
-    description: Mapped[str] = mapped_column(String(275), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
     ranking: Mapped[int] = mapped_column(Integer, nullable=False)
     review: Mapped[str] = mapped_column(String, nullable=False)
