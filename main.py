1510b23b9f56c8aed7fe72bdbe7b1d4583f4d596
Arie Knoester <arikstr@gmail.com>
2024-02-05 14:00:20 -0600

    Records in Movie table display in index.html

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
+from models.movie import Movie, db
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -14,17 +12,47 @@ import os
 load_dotenv(".env")
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///movies.db'
 Bootstrap5(app)
+db.init_app(app)
 
-# CREATE DB
+with app.app_context():
+    db.create_all()
 
-
-# CREATE TABLE
+# Add a few example records to the db to start.
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an "
+#                     "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with "
+#                     "the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#
+#     second_movie = Movie(
+#         title="Avatar The Way of Water",
+#         year=2022,
+#         description="Set more than a decade after the events of the first film, learn the story of the Sully family ("
+#                     "Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each "
+#                     "other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#         rating=7.3,
+#         ranking=9,
+#         review="I liked the water.",
+#         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+#     )
+#     db.session.add(second_movie)
+#     db.session.commit()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = db.session.execute(db.select(Movie).order_by(Movie.ranking.desc())).scalars()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
